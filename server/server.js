1e0c5e29a0ab896f6b746ee21c97ae82db480de4
davidlamarcook <dcook1025@gmail.com>
2022-06-23 19:16:16 -0600

    rollbar added

--- server/server.js
+++ server/server.js
@@ -6,6 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'd2e3a70e955342bd94dcd607711e8b56',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
